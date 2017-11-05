2a79e4054e21bcddbed7c2e67a07a1ab33587b85
codylovell <codylovell94@gmail.com>
2017-11-05 05:25:31 -0500

    learning log

---
+++ learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Defines urls for learning_logs app"""
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
---
+++ learning_logs/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def index(request):
+    """the home page for learning_log"""
+    return render(request, 'learning_logs/index.html')
